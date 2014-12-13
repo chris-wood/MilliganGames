7feb3265561d8af7dab2f9e0dd4a78e8493f0b75
Christopher Wood <christopherwood07@gmail.com>
2014-12-13 15:42:17 -0800

    More ebay integration

--- public/js/main.js
+++ public/js/main.js
@@ -1,38 +1,62 @@
 
-var rcrApp = angular.module('rcr',[]);
-rcrApp.controller('EbayController', ['$scope', '$http', function($scope, $http) {
-	// Construct the request
-	// Replace MyAppID with your Production AppID
-	var url = "http://svcs.ebay.com/services/search/FindingService/v1";
-		url += "?OPERATION-NAME=findItemsByKeywords";
-		url += "&SERVICE-VERSION=1.0.0";
-		url += "&SECURITY-APPNAME=UCIrvine-5b9a-4327-a068-22403f51a8dc";
-		url += "&GLOBAL-ID=EBAY-US";
-		url += "&RESPONSE-DATA-FORMAT=JSON";
-		// url += "&callback=_cb_findItemsByKeywords";
-		url += "&REST-PAYLOAD";
-		url += "&keywords=harry%20potter";
-		// url += "&paginationInput.entriesPerPage=3";
-		// url += urlfilter;
+// Construct the request for Pat's store contents
+// Replace MyAppID with your Production AppID
+var url = "https://api.ebay.com/wsapi";
+	// url += "?OPERATION-NAME=findItemsByKeywords";
+	// url += "&SERVICE-VERSION=1.0.0";
+	// url += "&SECURITY-APPNAME=******";
+	// url += "&GLOBAL-ID=EBAY-US";
+	// url += "&RESPONSE-DATA-FORMAT=JSON";
+	// // url += "&itemFilter.name=river_city_retro";
+	// // url += "&callback=_cb_findItemsByKeywords";
+	url += "&callback=?";
+	// url += "&REST-PAYLOAD";
+	// url += "&keywords=river_city_retro";
+	// // url += "&keywords=harry%20potter";
+	// // url += "&paginationInput.entriesPerPage=3";
+	// // url += urlfilter;
 
-		$http.get(url).
-			success(function(data, status, headers, config) {
-				var items = root.findItemsByKeywordsResponse[0].searchResult[0].item || [];
-				var html = [];
-				for (var i = 0; i < items.length; ++i) {
-					var item     = items[i];
-					var title    = item.title;
-					var pic      = item.galleryURL;
-					var viewitem = item.viewItemURL;
-					if (null != title && null != viewitem) {
-						console.log(pic);
-						console.log(viewitem);
-						console.log(title);
-					}
-				}
-				console.log("done");
-			}).
-			error(function(data, status, headers, config) {
-				console.log("an error occurred: " + data);
-			});
-}]);
+var requestString = '<GetSellerListRequest xmlns="urn:ebay:apis:eBLBaseComponents"><RequesterCredentials><eBayAuthToken>*****</eBayAuthToken></RequesterCredentials><UserID>river_city_retro</UserID><Pagination><EntriesPerPage>200</EntriesPerPage><PageNumber>1</PageNumber></Pagination><OutputSelector>ItemArray.Item.ItemID</OutputSelector><OutputSelector>ItemArray.Item.Quantity</OutputSelector><OutputSelector>ItemArray.Item.Title</OutputSelector><OutputSelector>ItemArray.Item.PrimaryCategory.CategoryID</OutputSelector><OutputSelector>ItemArray.Item.PrimaryCategory.CategoryName</OutputSelector></GetSellerListRequest>';
+
+//  = '<GetSellerListRequest xmlns="urn:ebay:apis:eBLBaseComponents">
+//   <RequesterCredentials>
+//     <eBayAuthToken>******</eBayAuthToken>
+//   </RequesterCredentials>
+//   <ErrorLanguage>en_US</ErrorLanguage>
+//   <WarningLevel>High</WarningLevel>
+//   <StartTimeFrom>2013-06-01T21:59:59.005Z</StartTimeFrom> 
+//   <StartTimeTo>2013-09-26T21:59:59.005Z</StartTimeTo>
+//   <EndTimeFrom>2013-09-26</EndTimeFrom>
+//   <EndTimeTo>2013-11-26</EndTimeTo>
+//   <GranularityLevel>Coarse</GranularityLevel>
+//   <UserID>river_city_retro</UserID>
+//   <Pagination>
+//     <EntriesPerPage>200</EntriesPerPage>
+//     <PageNumber>1</PageNumber>
+//   </Pagination>
+//   <OutputSelector>ItemArray.Item.ItemID</OutputSelector>
+//   <OutputSelector>ItemArray.Item.Quantity</OutputSelector>
+//   <OutputSelector>ItemArray.Item.Title</OutputSelector>
+//   <OutputSelector>ItemArray.Item.PrimaryCategory.CategoryID</OutputSelector>
+//   <OutputSelector>ItemArray.Item.PrimaryCategory.CategoryName</OutputSelector>
+// </GetSellerListRequest>';
+// var xmlData = jQuery.parseXML(requestString);
+
+$().ready(function () {
+	$.ajax({url: url, data: requestString, success: function (rootData) { 
+		console.log(rootData);
+		var items = rootData.findItemsByKeywordsResponse[0].searchResult[0].item || [];
+		for (var i = 0; i < items.length; ++i) {
+			var item     = items[i];
+			var title    = item.title;
+			var pic      = item.galleryURL;
+			var viewitem = item.viewItemURL;
+			if (title != null && viewitem != null) {
+				console.log(pic);
+				console.log(viewitem);
+				console.log(title);
+			}
+		}
+		console.log("done");
+	}, dataType: 'xml'});
+});
